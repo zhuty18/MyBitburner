23aabf321d45c32d13960761be70d3a2395f544b
zhuty <13718054285@163.com>
2022-03-11 19:14:20 +0800

    优化

--- script/home_run.js
+++ script/home_run.js
@@ -5,7 +5,7 @@ export async function main (ns) {
     ns.exec("scan.js", "home", 1)
     var ram = ns.getServerMaxRam("home")
     var l = ["foodnstuff", "zer0", "sigma-cosmetics", "max-hardware", "joesguns", "nectar-net", "hong-fang-tea", "harakiri-sushi", "iron-gym", "neo-net"]
-    for (var i = 0; i < l.length && (i + 1) * 1024 <= ram; l++) {
+    for (var i = 0; ((i < l.length) && ((i + 1) * 1024 <= ram)); i++) {
         ns.exec("easy_loop.js", "home", 1, l[i], 1020)
     }
 }
